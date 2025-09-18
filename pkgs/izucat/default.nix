a3b85cc07df7e9933addbe24c586f5cd9ef5818d
Amamiya Mion <50908468+AmamiyaMion@users.noreply.github.com>
2025-09-18 23:01:19 +0800

    izucat: init package

--- pkgs/izucat/default.nix
+++ pkgs/izucat/default.nix
@@ -12,9 +12,9 @@ rustPlatform.buildRustPackage rec {
     owner = "Souiken";
     repo = "izucat";
     rev = "v${version}";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-b4XpQB4kUWhQwQy7SwgWCSI9WimTEY9YlbrKD9L3R5M=";
   };
-  cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  cargoHash = "sha256-u+IOgPc9llFDA++nFSxWg3Mp4bvSNt1ttRbBH1ewgdE=";
   meta = {
     description = "A program that can recursively concatenate (cat) text and binary files in a path and/or command/stdin output to typst.";
     homepage = "https://github.com/Souiken/izucat";
